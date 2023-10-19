2721db90ac0443661a1b9ea5e5d040c02db23a4f
son <sontng@zen-s.com>
2023-10-19 10:15:17 +0700

    [HP2] - Implement - home page 2

--- src/pages/index-2.tsx
+++ src/pages/index-2.tsx
@@ -3,99 +3,31 @@ import Layout from "../components/layout/Layout";
 import Newsletter from "../components/elements/Newsletter";
 import CustomerFeedback from "../components/elements/CustomerFeedback";
 import { PricingData1 } from "../Data/pricingData";
+import { data } from "../Data/ShopData";
 import { PriceTable } from "../components/elements/PriceTable";
+import ProductCategory from "../components/elements/ProductCategory";
+
 
 
 function Index2(): JSX.Element {
     return (
         <>
             <Layout>
-                <div>
-                    <section className="section-box">
-                        <div className="banner-hero banner-2 overflow-hidden">
-                            <div className="container">
-                                <div className="row mb-50">
-                                    <div className="col-lg-7 mt-50">
-                                        <span className="tag-1 bg-color-3 color-white">Moirai</span>
-                                        <h1 className="text-display-3 mt-30">
-                                            Unveiling the{" "}
-                                            <span className="color-gray-blue">latest fashion</span>{" "}
-                                            trend in global
-                                        </h1>
-                                        <p className="text-body-lead-large color-gray-500 mt-40 pr-40">
-                                            The lastest trend is waiting for you
-                                        </p>
-                                        <div className="mt-40">
-                                            <Link
-                                                to="/#"
-                                                className="btn btn-black shape-square icon-arrow-right-white">
-                                                Get Start
-                                            </Link>
-                                            <Link
-                                                to="/#"
-                                                className="btn btn-link icon-triangle-2 color-gray-900 ml-40">
-                                                How it works
-                                            </Link>
-                                        </div>
-                                        <div className="mt-50">
-                                            <div className="row">
-                                                <div className="col-lg-3 col-sm-4 col-4">
-                                                    <h3 className="text-heading-4 color-gray-900 mb-15">
-                                                        <span className="count">5000</span>+
-                                                    </h3>
-                                                    <p className="text-body-text-md color-gray-500">
-                                                        Product
-                                                    </p>
-                                                </div>
-                                                <div className="col-lg-3 col-sm-4 col-4">
-                                                    <h3 className="text-heading-4 color-gray-900 mb-15">
-                                                        <span className="count">756</span>+
-                                                    </h3>
-                                                    <p className="text-body-text-md color-gray-500">
-                                                        Happy Client
-                                                    </p>
-                                                </div>
-                                                <div className="col-lg-4 col-sm-4 col-4">
-                                                    <h3 className="text-heading-4 color-gray-900 mb-15">
-                                                        <span className="count">100</span>%
-                                                    </h3>
-                                                    <p className="text-body-text-md color-gray-500">
-                                                        Client Satisfaction
-                                                    </p>
-                                                </div>
-                                            </div>
-                                        </div>
-                                    </div>
-                                    <div className="col-lg-5 d-none d-lg-block">
-                                        <div className="banner-imgs">
-                                            <div className="block-1 shape-2">
-                                                <img
-                                                    src="/assets/imgs/page/homepage2/balance.png"
-                                                    alt="Moirai"
-                                                />
-                                            </div>
-                                            <div className="block-2 shape-3">
-                                                <img
-                                                    src="/assets/imgs/page/homepage2/payment.png"
-                                                    alt="Moirai"
-                                                />
-                                            </div>
-                                            <div className="img-responsive shape-1">
-                                                <img
-                                                    alt="Moirai"
-                                                    src="/assets/imgs/page/homepage2/banner.png"
-                                                />
-                                            </div>
-                                        </div>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-                    </section>
+                <section className="section-box">
+                    <div>
+                        <h2>Unveiling the latest furniture
+                            trend in global
+                        </h2>
+                        <p>Unveiling the latest furniture
+                            trend in global
+                        </p>
+                        <button>View more</button>
+                    </div>
+                </section>
                     <div className="section-box overflow-visible mt-90">
                         <div className="container">
                             <h2 className="text-heading-3 text-center color-gray-900 mb-60">
-                                Trusted by the world’s fashion companies
+                            SHOP BY COLLECTION
                             </h2>
                             <div className="row justify-content-md-center">
                                 <div className="col-lg-2 col-md-3 col-sm-4 col-6 text-center">
@@ -139,95 +71,60 @@ function Index2(): JSX.Element {
                                         <img
                                             alt="Moirai"
                                             className="w-100"
-                                            src="/assets/imgs/slider/logo/versace.svg"
-                                        />
-                                    </Link>
-                                </div>
-                                <div className="col-lg-2 col-md-3 col-sm-4 col-6 text-center">
-                                    <Link to="/#" className="item-logo box-hover-shadow hover-up">
-                                        <img
-                                            alt="Moirai"
-                                            className="w-100"
+                                        src="/assets/imgs/slider/logo/versace.svg"
+                                    />
+                                </Link>
+                            </div>
+                            <div className="col-lg-2 col-md-3 col-sm-4 col-6 text-center">
+                                <Link to="/#" className="item-logo box-hover-shadow hover-up">
+                                    <img
+                                        alt="Moirai"
+                                        className="w-100"
                                             src="/assets/imgs/slider/logo/burberry.svg"
                                         />
                                     </Link>
                                 </div>
                             </div>
-                        </div>
-                    </div>
-                    <section className="section-box mt-100">
-                        <div className="container mt-60">
-                            <div className="row">
-                                <div className="col-lg-6 col-sm-12 block-img-we-do">
-                                    <div className="inner-image">
-                                        <img
-                                            className="bdrd-16 img-responsive"
-                                            src="/assets/imgs/page/homepage2/img-2.png"
-                                            alt="Moirai"
-                                        />
-                                        <div className="block-chart rounded-4 overflow-hidden">
-                                            <img
-                                                src="/assets/imgs/page/homepage2/chart.png"
-                                                alt="Moirai"
-                                            />
-                                        </div>
-                                    </div>
-                                </div>
-                                <div className="col-lg-6 col-sm-12 block-we-do-2">
-                                    <span className="tag-1 bg-6 color-green-900">Overview</span>
-                                    <h3 className="text-heading-1 mt-30">About Us</h3>
-                                    <p className="text-body-lead-large color-gray-600 mt-30">
-                                        We facilitate the creation of strategy and design
-                                    </p>
-                                    <div className="list-icons mt-50">
-                                        <div className="item-icon none-bd">
-                                            <span className="icon-left">
-                                                <img
-                                                    src="/assets/imgs/page/homepage2/icon-company.svg"
-                                                    alt="Moirai"
-                                                />
-                                            </span>
-                                            <h4 className="text-heading-4">About the Company</h4>
-                                            <p className="text-body-excerpt color-gray-600 mt-15">
-                                                Our company is a leading fashion enterprise dedicated to
-                                                providing exceptional clothing and accessories.
-                                            </p>
-                                        </div>
-                                        <div className="item-icon none-bd">
-                                            <span className="icon-left">
-                                                <img
-                                                    src="/assets/imgs/page/homepage2/icon-history.svg"
-                                                    alt="Moirai"
-                                                />
-                                            </span>
-                                            <h4 className="text-heading-4">History of Information</h4>
-                                            <p className="text-body-excerpt color-gray-600 mt-15">
-                                                Our company has a rich and inspiring history. It all
-                                                started [insert specific information about the
-                                                company&apos;s origin, such as the year and
-                                                circumstances of establishment].
-                                            </p>
-                                        </div>
-                                        <div className="item-icon none-bd">
-                                            <span className="icon-left">
-                                                <img
-                                                    src="/assets/imgs/page/homepage2/icon-mission.svg"
-                                                    alt="Moirai"
-                                                />
-                                            </span>
-                                            <h4 className="text-heading-4">Company Mission</h4>
-                                            <p className="text-body-excerpt color-gray-600 mt-15">
-                                                At our company, our mission is to empower individuals to
-                                                express their unique personalities through fashion. We
-                                                believe that clothing goes beyond mere functionality—it
-                                                is a powerful form of self-expression.
-                                            </p>
-                                        </div>
-                                    </div>
-                                </div>
+                        <div className="row justify-content-md-center">
+                            <div className="col-lg-4 col-md-3 col-sm-4 col-6 text-center">
+                                <Link to="/#" className="item-logo box-hover-shadow hover-up">
+                                    <img
+                                        alt="Moirai"
+                                        className="w-100"
+                                        src="/assets/imgs/slider/logo/dg.svg"
+                                    />
+                                </Link>
+                            </div>
+                            <div className="col-lg-4 col-md-3 col-sm-4 col-6 text-center">
+                                <Link to="/#" className="item-logo box-hover-shadow hover-up">
+                                    <img
+                                        alt="Moirai"
+                                        className="w-100"
+                                        src="/assets/imgs/slider/logo/dg.svg"
+                                    />
+                                </Link>
+                            </div>
+                            <div className="col-lg-4 col-md-3 col-sm-4 col-6 text-center">
+                                <Link to="/#" className="item-logo box-hover-shadow hover-up">
+                                    <img
+                                        alt="Moirai"
+                                        className="w-100"
+                                        src="/assets/imgs/slider/logo/dg.svg"
+                                    />
+                                </Link>
                             </div>
                         </div>
-                    </section>
+                    </div>
+                </div>
+                <div className="container mt-60 mb-100">
+                    <div className="">
+                        <div>LATEST</div>
+                        <div>BEST SELLING</div>
+                        <div>SPECIAL</div>
+                    </div>
+                    <ProductCategory data={data} />
+                </div>
+
                     <section className="section-box mt-100">
                         <div className="container mt-60">
                             <div className="row">
@@ -456,8 +353,7 @@ function Index2(): JSX.Element {
                         </div>
                         <PriceTable priceData={PricingData1} />
                     </section>
-                    <Newsletter newsletterStyle="style-1" />
-                </div>
+                <Newsletter newsletterStyle="style-1" />
             </Layout>
         </>
     );
